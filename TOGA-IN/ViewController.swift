52ffddd06d350ddc241893ee9daf183b792bdcbe
PramahadiTamaPutra <pramahaditamaputra@outlook.com>
2019-04-25 16:03:42 +0700

    Initial Commit

---
+++ TOGA-IN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TOGA-IN
+//
+//  Created by Pramahadi Tama Putra on 25/04/19.
+//  Copyright © 2019 Pramahadi Tama Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
